f0d5de61bf621cd009e5b0e312e34677616d9e09
FET0 <fuhadtadese@gmail.com>
2025-12-14 15:44:18 +0100

    Yaml troubleshoot 3

--- src/MyWebApp/Program.cs
+++ src/MyWebApp/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello, DevOps Tasks attempt after installing .NET dependencies after installing .NET dependencies");
+app.MapGet("/", () => "Hello, DevOps Tasks attempt after installing .NET dependencies and also setting paths");
 
 app.Run();
