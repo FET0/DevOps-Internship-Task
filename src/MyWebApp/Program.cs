3b0f956d1630f5f6f728cf713edfe1aff87d67c8
FET0 <fuhadtadese@gmail.com>
2025-12-14 09:20:21 +0100

    agent test

--- src/MyWebApp/Program.cs
+++ src/MyWebApp/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello, DevOps Recruiter!");
+app.MapGet("/", () => "Hello, DevOps Task!");
 
 app.Run();
