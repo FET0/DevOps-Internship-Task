5e0fc739aff25732ff514094a40425998b816adc
FET0 <fuhadtadese@gmail.com>
2025-12-15 11:02:39 +0100

    Demonstration

--- src/MyWebApp/Program.cs
+++ src/MyWebApp/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello, DevOps Task demonstration");
+app.MapGet("/", () => "Hello, DevOps World ");
 
 app.Run();
